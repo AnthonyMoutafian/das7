a1861eb6e465cab4c3f2370ba389e7f666f4e91a
Anthony Moutafian <anthonyvache2007@gmail.com>
2022-11-22 15:59:57 +0400

    exercise2

---
+++ main.js
@@ -0,0 +1,10 @@
+function main(tiv){
+   return function astichanhashvox(astichan) {
+       if (isNaN(astichan)) return "chgrel string"
+       if (isNaN(tiv)) return "chgrel string"
+       return tiv**astichan
+   }
+}
+
+let a = main(prompt("nermucel tiv"))(prompt("barcracnel astichan"))
+console.log(a);
